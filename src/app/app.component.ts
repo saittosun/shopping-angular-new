73829fb1f18a3b5ca8d27ee120111544790c01e3
tostos <saittosun2@gmail.com>
2020-05-12 10:59:37 +0200

    routing is finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   loadedFeature = 'recipe';
-  
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
+
+
 }
