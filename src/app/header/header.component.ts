a116e091df8bc64a7896a8a076028d515bbac5fd
tostos <saittosun2@gmail.com>
2020-05-12 07:36:07 +0200

    adding navigation to the app

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,12 +6,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  // tslint:disable-next-line:max-line-length
-  // we have to attach something to this property to make it listenable from the parent component, @output enables us to use this event or to listen to it from the parent component.
-  @Output() featureSelected = new EventEmitter<string>();
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
+
   constructor() { }
 
   ngOnInit(): void {
