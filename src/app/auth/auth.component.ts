9c102a5b528ce2fe4c57a47ce4b87fb91a07fb6c
tostos <saittosun2@gmail.com>
2020-05-22 12:08:17 +0200

    switching between auth modes

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  isLoginMode = true;
 
   constructor() { }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 
   ngOnInit(): void {
   }
