a27472e6677e3157da638fed614cbe53c44f4332
tostos <saittosun2@gmail.com>
2020-06-03 16:29:50 +0200

    setting up auth reducer & actions

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from '../user.model';
+import * as AuthActions from './auth.actions';
 
 export interface State {
   user: User;
@@ -6,8 +7,31 @@ export interface State {
 
 const initialState = {
   user: null
-}
+};
 
-export function authReducer(state = initialState, action) {
-  return state;
+export function authReducer(
+  state = initialState,
+  // tslint:disable-next-line:max-line-length
+  // Now this double name might be confusing but again, the first part here is just this container name of all the things that are exported by the file and then with a dot we accessed the different exported things, like our string identifiers or this auth actions union type.
+  action: AuthActions.AuthActions) {
+  switch (action.type) {
+    case AuthActions.LOGIN:
+      const user = new User(
+        action.payload.email,
+        action.payload.userId,
+        action.payload.token,
+        action.payload.expirationDate
+      );
+      return {
+        ...state,
+        user
+      };
+    case AuthActions.LOGOUT:
+      return {
+        ...state,
+        user: null
+      }
+    default:
+      return state;
+  }
 }
